5e15bd89307102b8ab541b0b025cdda25c5c9057
Jerome <jerome@hikashop.com>
2014-09-28 18:35:01 +0200

    Create example_configuration.php

---
+++ example_configuration.php
@@ -0,0 +1,4 @@
+<?php
+/*
+ * The HTML of the backend specifics configuration can be listed here. You don't need that file if you use the $pluginConfig attribute of the main PHP file of the plugin. But if you need custom type of fields which are not supported by the system, like we do for some plugins, you can use this file instead.
+ */
